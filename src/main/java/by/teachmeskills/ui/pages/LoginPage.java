5006da60162d3c005401c59b66a181b314084047
viktoria-aparina <107716888+viktoria-aparina@users.noreply.github.com>
2022-09-08 12:58:16 +0300

    Update LoginPage.java

--- src/main/java/by/teachmeskills/ui/pages/LoginPage.java
+++ src/main/java/by/teachmeskills/ui/pages/LoginPage.java
@@ -27,6 +27,7 @@ public class LoginPage extends BasePage {
 
     public LoginPage open() {
         Selenide.open("/login");
+        Configuration.browserSize = "1920х1080";
         return this;
     }
 
